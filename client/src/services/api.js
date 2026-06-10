7779135cba69e4eeb461c8d3d33fd02229eef79a
Deeksha-m19 <deekshamallikarjuna19@gmail.com>
2026-06-10 16:23:13 +0530

    fixed api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://smartcampus-h5xz.onrender.com"
+    baseURL: "https://smartcampus-h5xz.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
